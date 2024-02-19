538c2017c14a1ca39bfa9cd9c3e81ed64e90972b
Peter Sutton <p.sutton@uq.edu.au>
2024-02-19 19:55:29 +1000

    Updated magic number example to remove NEWLINE

--- examples/magic.c
+++ examples/magic.c
@@ -1,10 +1,8 @@
-#define NUM LETTERS IN ALPHABET 26
-#define NEWLINE '\n'
+// Constants can be defined using #define or "const variables"
+#define NUM_LETTERS_IN_ALPHABET 26
 const int minPasswordLength = 8;
-for (int i = 1; i <= NUM LETTERS IN ALPHABET; i++) {
-    //...
-}
-if (name[0] == NEWLINE) {
+
+for (int i = 1; i <= NUM_LETTERS_IN_ALPHABET; i++) {
     //...
 }
 if (strlen(passwd) < minPasswordLength) {
