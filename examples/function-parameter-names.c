a1793ae9924996c04127db95ca6b7479575c401c
Peter Sutton <p.sutton@uq.edu.au>
2024-02-22 10:52:29 +1000

    Updated example to use __attribute__((unused))

--- examples/function-parameter-names.c
+++ examples/function-parameter-names.c
@@ -10,12 +10,13 @@ void fn1(int i, char* s, double d)
 // (Though the names should be more meaningful!)
 void fn2(int a, char* s, double d); // declaration
 
-void fn2(int a, char* s, double /* d */)
-{ // definition, d not used in function
+void fn2(int a, char* s, double d __attribute__((unused)))
+{ // definition, d not used in function, but function should almost certainly be
+  // rewritten to omit the parameter.
   // code here
 }
 
-int main(int /* argc */, char* argv[])
+int main(int argc __attribute__((unused)), char* argv[])
 { // argv used but not argc
   // code here
 }
