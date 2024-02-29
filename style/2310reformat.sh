ba076b406592af91c2af5082ea85ae5e52da71dc
Peter Sutton <p.sutton@uq.edu.au>
2024-02-29 20:04:21 +1000

    Style checking script

---
+++ style/2310reformat.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 2310reformat.sh
+#
+# Script that runs clang-format with the CSSE2310/7231 specific
+# .clang-format configuration and fixes the format of the file.
+#	
+
+# Check if any arguments are given
+if [ ! "$1" ] ; then 
+    echo "Usage: 2310format.sh filename ..." >&2
+    exit 1
+fi
+
+/local/courses/csse2310/bin/csse2310logusage
+
+# Iterate over all the arguments. If readable then reformat
+while [ -r "$1" ] ; do
+    if ! /usr/bin/clang-format \
+	    --style=file:/local/courses/csse2310/resources/.clang-format \
+	    -i "$1" ; then
+	echo "Reformatting failed on file '$1' - aborting" >&2
+	exit 2
+    fi
+    shift
+done
+# Check if any files left
+if [ "$1" ] ; then 
+    # At least one file left - we must not have been able to open it
+    echo "Unable to open file '$1' - aborting" >&2
+    exit 3
+fi
---
+++ style/2310stylecheck.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# 2310stylecheck.sh [--globalOK] [file ...]
+#
+# Script that runs clang-format and clang-tidy to report any issues. If no
+# filenames are given then all .c and .h files in the current directory are
+# checked.
+
+/local/courses/csse2310/bin/csse2310logusage
+
+configdir=/local/courses/csse2310/resources
+shopt -s nullglob
+
+if [ "$1" = "--globalOK" ] ; then
+    extraArg="--checks=-cppcoreguidelines-avoid-non-const-global-variables"
+    shift
+else
+    extraArg=""
+fi
+function checkfile() {
+    if [ -r "$1" ] ; then
+	echo "---------- Running clang-format on $1"
+	/usr/bin/clang-format --style=file:${configdir}/.clang-format \
+		--dry-run "$1" 2>&1
+	echo "---------- Running clang-tidy on $1"
+	/usr/bin/clang-tidy -p /tmp --quiet \
+		--config-file=${configdir}/.clang-tidy $extraArg "$1" 2>/dev/null
+    else
+	echo "---------- Can not open '$1' - not checking">&2
+    fi
+}
+
+# Check if any arguments are given
+if [ "$#" = 0 ] ; then
+    # No arguments given - get the .c and .h files
+    for i in *.[hc] ; do
+        checkfile "$i"
+    done
+else
+    while [ "$1" ] ; do
+        checkfile "$1"
+        shift
+    done
+fi
