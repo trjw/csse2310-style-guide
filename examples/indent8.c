2f64ff21c5f74afe9c94a73df374dec1b9ade10a
Peter Sutton <p.sutton@uq.edu.au>
2024-03-04 12:59:04 +1000

    Added const to example

--- examples/indent8.c
+++ examples/indent8.c
@@ -1,7 +1,7 @@
-const char* longMessageVariableName1
+const char* const longMessageVariableName1
         = "This is an example of a string constant that fits on the next line";
-const char* longMessageVariableName2 = "This approach is also OK even "
-                                       "if the constant would fit on one line";
-const char* message3
+const char* const longMessageVariableName2 = "This approach is also OK even if "
+                                             "the string would fit on one line";
+const char* const message3
         = "This is another example of a long string constant that must be split"
           " over multiple lines";
