dc53980f4ab1fd3cfd94f97a5bfb09d33b11691b
Peter Sutton <p.sutton@uq.edu.au>
2026-02-04 22:25:40 +1000

    header guard example

---
+++ examples/header-guard-example.h
@@ -0,0 +1,12 @@
+/** @list.h
+ * @author Jane Doe
+ * @brief This file contains the declaration of a simple linked list 
+ * structure and its associated functions.
+ */
+
+#ifndef LIST_H
+#define LIST_H
+
+// ... definitions omitted for brevity ...
+
+#endif // LIST_H
