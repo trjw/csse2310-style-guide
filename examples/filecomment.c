1dd8abdaa41b6236f5517b354c3e329e85d936eb
Peter Sutton <p.sutton@uq.edu.au>
2026-02-09 22:09:34 +1000

    Still in progress - added doxygen icond and beginnings of comment requirements

---
+++ examples/filecomment.c
@@ -0,0 +1,9 @@
+/** 
+ * @file program.c
+ * @author Peter Sutton
+ * @brief A simple program to demonstrate file comments - this and other 
+ *      commands besides @file, @author and @ai are optional for CSSE2310.
+ * @ai Wrote Code
+ * @ai Wrote Comments
+ * @ai Debugging
+ */
--- examples/vardecls.c
+++ examples/vardecls.c
@@ -4,7 +4,7 @@ void fn(int a, int b)
               // the variables aren't being initialised with non-
               // constant expressions
     int x = 0, y = 1; // Initialisations to constants can be put on one line
-    int product = a * b; // Declaring one variable and initialising it is fine
+    int product = a * b; // Declaring one variable with a non-constant initialiser is fine
     // Don't do this: int sum = a + b, diff = a-b;
     // Do this (or declare and initialise separately):
     int sum = a + b;
