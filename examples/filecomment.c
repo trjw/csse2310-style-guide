bd4abf74159b0fdfd70e0323a868f20a5d2199eb
Peter Sutton <p.sutton@uq.edu.au>
2026-02-12 16:15:07 +1000

    Updated example

--- examples/filecomment.c
+++ examples/filecomment.c
@@ -1,11 +1,13 @@
 /** 
  * @file program.c
- * @author Peter Sutton
- * @brief A simple program to demonstrate file comments - this and other 
- *      commands besides @file, @author and @ai are optional for CSSE2310.
+ * @author Alex Bloggs
+ * @brief A simple program to demonstrate file comments - this and other
+ *      commands besides @file, @author and @ai are optional for CSSE2310 
+ *      file comments.
  * @ai Wrote Code
  * @ai Wrote Comments
  * @ai Debugging
  *
- * Additional text can be present in the comment if desired, but is not required.
+ * Additional descriptive text can be present in the comment if desired, but 
+ * is not required.
  */
