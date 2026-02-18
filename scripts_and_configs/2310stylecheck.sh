9cd4451019a3856a89b6d5d1b2ecbea9b4680573
Peter Sutton <p.sutton@uq.edu.au>
2026-02-18 10:32:36 +1000

    Add PATH definition. Comment out function declarations in .h files also - force function comments to be with function definitions

--- scripts_and_configs/2310stylecheck.sh
+++ scripts_and_configs/2310stylecheck.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # 2310stylecheck.sh [--globalOK] [file ...]
 #
+# Written by Peter Sutton. Updated Feb 2026.
+#
 # Script that checks the style of the given files against the requirements
 # of the CSSE2310/CSSE7231 style guide. If no files are specified, then the
 # script checks all .c and .h files in the current directory
 
+export PATH="/local/courses/csse2310/bin:/usr/bin:$PATH"
 configdir=/local/courses/csse2310/resources
 clanglib=/local/courses/csse2310/lib/libClangTidy2310.so
 shopt -s nullglob
@@ -17,17 +20,20 @@ function checkfile() {
         echo "$1" | grep -E -q '(^[A-Z])|_' && echo "Warning: inappropriate source filename: $1"
         LC_ALL=C grep -n -P '[^\x00-\x7F]' "$1" | sed -e 's/^/Warning: Non ASCII character(s) at /'
         echo "---------- Checking $1 compiles by itself"
-        if ! /usr/bin/gcc $compilerArgs -c -o /dev/null "$1" 2>&1
+        if ! gcc $compilerArgs -c -o /dev/null "$1" 2>&1 ; then
             echo "---------- Found compilation errors - not checking other style aspects" >&2
             compileErrorFound=1
         else
             echo "---------- Running clang-format on $1"
-            /usr/bin/clang-format --style=file:${configdir}/.clang-format \
+            clang-format --style=file:${configdir}/.clang-format \
                     --dry-run "$1" 2>&1
             echo "---------- Running clang-tidy on $1"
-            /usr/bin/clang-tidy --quiet --load=${clanglib} \
+            clang-tidy --quiet --load=${clanglib} \
                     --config-file=${configdir}/.clang-tidy $extraClangTidyArg \
                     "$1" -- 2>/dev/null
+            echo "---------- Checking for comments inside functions for $1"
+            2310checkfunctioninternalcomments.sh "$1"
+        fi
     else
 	echo "---------- Can not open '$1' - not checking">&2
     fi
@@ -81,10 +87,12 @@ if [ "$compileErrorFound" = 0 ] ; then
     mkdir -p ${TMP}
     # Copy all the files to a temporary directory
     for i in "${files[@]}" ; do
-        /usr/bin/clang-query --use-color=0 -c 'enable output print' \
+        # Comment out function declarations and const variables - no comments
+        # are expected for these
+        clang-query --use-color=0 -c 'enable output print' \
                 -c 'set traversal IgnoreUnlessSpelledInSource' \
                 -c 'm varDecl(isExpansionInMainFile(), hasGlobalStorage(), isDefinition(), hasType(isConstQualified()))' \
-                -c 'm functionDecl(unless(isDefinition()), isExpansionInMainFile(), isExpansionInFileMatching(".*\\.c"))' \
+                -c 'm functionDecl(unless(isDefinition()), isExpansionInMainFile())' \
                 "$i" -- |
             gawk 'BEGIN {infile=0; printf("1\n");}
                 /^\/.*\/'$1':/ {infile=1; next;}
@@ -95,9 +103,10 @@ if [ "$compileErrorFound" = 0 ] ; then
     done
     cd ${TMP}
 
-    /usr/bin/doxygen ${configDir}/.csse2310.doxyconfig
+    # Run doxygen to check the files
+    doxygen ${configdir}/.csse2310.doxyconfig
     # Output is now in warnings.txt - filter and sort - and change the 
-    # terminology
+    # terminology. We update the file names to just show the base name.
     sed -e "s@^${TMP}/@@" warnings.txt | sort -t: -k1,1 -k2,2n | sed '/Member.* of class/s/of class/of struct/;/Member.* of file/s/Member //;s/ of file / in file /;s/parameters of member/parameters of function/'
     rm -f -r ${TMP}
 fi
