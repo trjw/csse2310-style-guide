224e71c5b0c3a2bf8e0d8634d6939822cd2fe8ca
Peter Sutton <p.sutton@uq.edu.au>
2026-02-21 10:12:47 +1000

    Minor updates around @ai comments

--- examples/filecomment.c
+++ examples/filecomment.c
@@ -2,8 +2,8 @@
  * @file program.c
  * @author Alex Bloggs
  * @brief A simple program to demonstrate file comments - this and other
- *      commands besides @file, @author and @ai are optional for CSSE2310 
- *      file comments.
+ *      commands besides @file, @author, @ai and possibly @aitool are 
+ *      optional for CSSE2310 file comments.
  * @ai Wrote Code
  * @ai Wrote Comments
  * @ai Debugging
