79d57b9ae00d7df69042bc2473195c9d8516ee3a
Peter Sutton <p.sutton@uq.edu.au>
2026-02-24 10:34:51 +1000

    Added section about required macrom comments

---
+++ examples/macrocomment.c
@@ -0,0 +1,7 @@
+/// Macro to return the smallest of two numerical values (a and b)
+#define MINIMUM(a, b) (((a) < (b)) ? (a) : (b))
+
+#define MAXIMUM(a, b) (((a) > (b)) ? (a) : (b))
+///< Macro to return the largest of two numerical values (a and b)
+
+#define COURSE_NUMBER 2310
